9edbec870ed93120d5b19873239a27cd87570363
Nathan Henry Puch <npuch@purdue.edu>
2019-12-02 13:51:24 -0500

    Simple_Variables functionality

--- Main.java
+++ Main.java
@@ -2,12 +2,19 @@ import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
+import java.util.Hashtable;
+
 public class Main {
     static int pc = 0;
+    static int offset = 0; //for position on stack
+    static int varLoc = 0; //for location of var on stack
+    static Hashtable<String, Integer> intTable = new Hashtable<String, Integer>(); //holds variables contents
+    static Hashtable<String, Integer> varOffsetTable = new Hashtable<String, Integer>(); //holds var's offset
+    static Hashtable<Integer, String> offsetVarTable = new Hashtable<Integer, String>();
+    static ArrayList<Integer> stack = new ArrayList<Integer>(); 
     public static void reader(String fileName){
         BufferedReader br = null;
         ArrayList<byte[]> list = new ArrayList<byte[]>();
-        
         try {
             File file = new File(fileName);
             br = new BufferedReader(new FileReader(file));
@@ -18,12 +25,23 @@ public class Main {
                 String statement = tokens[0];
                 if(statement.equals("subr"))
                     subr(list);
-                else if(statement.equals("pushi"))
-                    pushi(list, tokens[1]);
+                else if(statement.equals("pushi")){
+                    Integer intObj = new Integer(tokens[1]);
+                    pushint(list, intObj);
+                }
+                else if(statement.equals("pushv")){
+                    pushv(list, tokens[1]);
+                }
                 else if(statement.equals("printi"))
                     printi(list, tokens[1]);
+                else if(statement.equals("printv"))
+                    printv(list, tokens[1]);
                 else if(statement.equals("ret"))
                     ret(list);
+                else if(statement.equals("decl"))
+                    decl(list, tokens[1]);
+                else if(statement.equals("popv"))
+                    popv(list, tokens[1]);
             }
             printList(list);
         } catch (final IOException e) {
@@ -41,12 +59,10 @@ public class Main {
         pushi(list, "1");
         byte[] opCode = {44};
         addOpCode(list, opCode);
-        pc++;
         byte[]opCode2 = {0};
         addOpCode(list, opCode2);
-        pc++;
     }
-
+    
     public static void printi(ArrayList<byte[]> list, String value){
         pushi(list, value);
         //146 printi
@@ -55,6 +71,20 @@ public class Main {
         addOpCode(list, opCode);
     }
 
+    //for pusing integers and adding to table
+    public static void pushint(ArrayList<byte[]> list, Integer value) {
+        //70 pushi
+        byte[] opCode = {70};
+        addOpCode(list, opCode);
+
+        //add int 
+        addInt(list, value.toString());
+
+        stack.add(value);
+        varLoc++;
+   }
+
+    //for pushing integers on stack and not adding to table
     public static void pushi(ArrayList<byte[]> list, String value){
          //70 pushi
          byte[] opCode = {70};
@@ -62,23 +92,67 @@ public class Main {
 
         //add int 
         addInt(list, value);
-        pc += 5;
+    }
+    public static void pushv(ArrayList<byte[]> list, String name){
+        //push var index to be pushed
+        Integer index = varOffsetTable.get(name);
+        pushi(list, Integer.toString(index));
+
+        //update varTable
+        
+        byte[] opCode = {74}; // pushvi 74
+        addOpCode(list, opCode);
+    }
+
+    public static void popv(ArrayList<byte[]> list, String name){ 
+        //offset on stack
+        Integer offsetVar = varOffsetTable.get(name);
+        pushi(list, Integer.toString(offsetVar));
+        
+        //set var equal to value in intTable
+        Integer value = stack.get(offsetVar);
+        intTable.put(name, value);
+        varLoc--;
+
+        byte[] opCode = {80}; //popv 80
+        addOpCode(list, opCode);
+    }
+
+    public static void printv(ArrayList<byte[]> list, String name){
+        //push var to index to be printed
+        Integer index = varOffsetTable.get(name);
+        pushi(list, Integer.toString(index));
+
+        //push pushvi opcode 74
+        byte[] opCode = {74};
+        addOpCode(list, opCode);
+
+        //push print opcode 146
+        byte b = (byte)146;
+        byte[] opCode2 = {b};
+        addOpCode(list, opCode2);
     }
 
     public static void ret(ArrayList<byte[]> list){
         pushi(list, "0");
         byte[] opCode = {77};  //77 popa 
         addOpCode(list, opCode);
-        pc++;
         byte[] opCode2 = {48}; //48 ret
         addOpCode(list, opCode2);
-        pc++;
+    }
+
+    public static void decl(ArrayList<byte[]> list, String name){
+        pushi(list, "0");
+        varOffsetTable.put(name, offset);
+        offsetVarTable.put(offset, name);
+        offset++;
     }
 
     public static void addOpCode(ArrayList<byte[]> list, byte[] opCode){
         String preCode = String.format("%02x", opCode[0]);
         System.out.print(preCode);
         list.add(opCode);
+        pc++;
     }
     public static void addInt(ArrayList<byte[]> list, String value){
         ByteBuffer byteBuffer = ByteBuffer.allocate(4);
@@ -91,12 +165,13 @@ public class Main {
             System.out.print(st);
         }
         list.add(bytesToAdd);
+        pc+=4;
     }
 
     public static void printList(ArrayList<byte[]> list){
         FileOutputStream fos;
         try{
-            fos = new FileOutputStream("C:\\Users\\Nathan\\Desktop\\basics_output.bin");
+            fos = new FileOutputStream("C:\\Users\\Nathan\\Desktop\\simple_variables_output.bin");
 
             for(byte[] b : list){
                 try{
